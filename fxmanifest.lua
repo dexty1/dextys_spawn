9a19c11b4e5f14019b39d885141e4a8b01f13575
dexty1 <77596597+dexty1@users.noreply.github.com>
2025-01-11 15:26:00 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Dexty'
+description 'Spawn player at last location automatic'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+dependency 'qb-core'
